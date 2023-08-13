810382cca26ca851521a3d106d3192fa144b4a76
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-08-13 20:26:15 +0300

    fix styles of navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -27,11 +27,11 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-            <li>
+            {/* <li>
               <a href="#features" className="page-scroll">
                 Features
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#about" className="page-scroll">
                 About
@@ -52,11 +52,11 @@ export const Navigation = (props) => {
                 Testimonials
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#team" className="page-scroll">
                 Team
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#contact" className="page-scroll">
                 Contact
