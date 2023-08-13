c4cee20fc16a8da65a1924158888ee02a38840c3
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-08-13 20:18:03 +0300

    fix styles of header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = (props) => {
   return (
-    <header id="header">
+    <header id="header" >
       <div className="intro">
         <div className="overlay">
           <div className="container">
